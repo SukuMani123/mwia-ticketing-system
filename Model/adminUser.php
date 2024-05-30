c2fc8dd3d54bb616eb2c550c2341dae00470c66d
Ramesh <ramesh@and-charge.me>
2024-05-30 12:35:25 +0200

    data model

---
+++ Model/adminUser.php
@@ -0,0 +1,33 @@
+<?php
+require_once ('../Networking/DatabaseConnect.php');
+
+class adminUser {
+   
+    public $id;
+    public $name;
+    public $email;
+
+    // Constructor to easily create User objects
+    public function __construct($data = null) {
+        if ($data) {
+            $this->id = $data['id'] ?? null;
+            $this->name = $data['name'] ?? null;
+            $this->email = $data['email'] ?? null;
+        }
+    }
+
+    public function selectUser($email, $password){
+        // Select query example
+        $db =  new DatabaseConnect();
+        $db->query("SELECT * FROM mwiaTicketingSystem.mwiaadmin WHERE email = :id AND password = :pass");
+        $db->bind(':id', $email);
+        $db->bind(':pass', $password);
+        $results = $db->single();
+        
+        $users = new adminUser($results);
+
+        $db->closeConnection();
+        return $users;
+    }
+
+}
